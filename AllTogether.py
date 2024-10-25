acce08ad015bf96c6dd41106e5916caaaf32e051
Kev-zw <kh613@kent.ac.uk>
2024-10-25 15:15:20 +0000

    commit

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,11 @@
 ### Part Four -- your code goes here. 
+import random
+numbers = [random.randint(1, 100) for _ in range (10)]
+print("Original list:", numbers)
+
+index = 0
+while index < len(numbers):
+    if numbers[index] % 2 == 0: numbers.pop(index)
+    else: index =+1 
+if numbers: print ("List of odd numbers:", numbers)
+else: print("No odd numbers found.")
--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,17 @@
 ### Part Two -- your code goes here. 
+import random 
+number_to_guess = random.randint(1, 100)
+guess = None 
+print("Would you like to play a game?")
+print("I'm thinking of a number between 1 and 100.")
+
+while guess != number_to_guess:
+    try : guess = int(input("Enter Your Guess: "))
+    except ValueError:
+        print("Please enter a valid number.")
+    if guess < number_to_guess: 
+        print("Too low! Try again.")
+    elif guess > number_to_guess:
+        print("Too high! Try again.")
+    else: print ("Congratulations! You guessed the correct number!")
+    
